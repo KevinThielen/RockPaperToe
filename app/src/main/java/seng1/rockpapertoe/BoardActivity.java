502066bc86a6a3b3966874208302d2157942dcef
Antonios K <mtxcm22@gmail.com>
2016-05-03 03:06:30 +0200

    Added Async Task for Loading Highscore Values and added MyHighscore-View if not in Top 10

--- app/src/main/java/seng1/rockpapertoe/BoardActivity.java
+++ app/src/main/java/seng1/rockpapertoe/BoardActivity.java
@@ -1,6 +1,8 @@
 package seng1.rockpapertoe;
 
+import android.app.ProgressDialog;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
@@ -11,6 +13,7 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class BoardActivity extends AppCompatActivity {
@@ -19,92 +22,113 @@ public class BoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        // Highscore Object for every Client
         hmt = new HighscoreMockupTest();
+
+        //Asynchron Task
+        new HighscoreDataTask().execute();
+
+        sessionID = new Random().nextInt(12);
     }
 
-    private void hierVersucheIchRowsZuErstellen(View v, ArrayList<HighscoreMockup> top10){
+    int sessionID;
+
+    private void hierVersucheIchRowsZuErstellen(ArrayList<HighscoreMockup> top10){
+
         TableLayout tl = (TableLayout) findViewById(R.id.fullscreen_content);
+        Boolean highscoreInTop10 = false;
 
-        //szenario 1
-        //-------------------------------------
+        int size = 0;
 
+        if(top10.size() < 10)
+            size = top10.size();
+        else
+            size = 10;
 
-        for(int i = 0; i < top10.size(); i++){
+        for(int i = 0; i < size; i++) {
             TableRow tr = new TableRow(this);
 
             TextView tv = new TextView(this);
             tv.setTextSize(20);
             tv.setText("" + top10.get(i).getRanking());
-            tv.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL);
-
-            //tl.addView(tr,0);
-            //tl.removeView(tr);
+            tv.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
 
             TextView tv2 = new TextView(this);
             tv2.setTextSize(20);
             tv2.setText(top10.get(i).getName());
             tv2.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
 
-            //tl.addView(tr,1);
-            //tl.removeView(tr);
-
             TextView tv3 = new TextView(this);
             tv3.setTextSize(20);
             tv3.setText("" + top10.get(i).getScore());
             tv3.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
 
-            if(top10.get(i).getId() == 1) {
+            if (top10.get(i).getId() == sessionID) {
+                highscoreInTop10 = true;
                 tr.setBackgroundColor(Color.RED);
+                tr.getBackground().setAlpha(127);
             }
 
             tr.addView(tv);
             tr.addView(tv2);
             tr.addView(tv3);
             tl.addView(tr);
-
-            //tl.removeView(tr);
         }
 
-        /*
+        if(!highscoreInTop10) {
+            Log.d("Meine Randomzahl: ", ""+sessionID);
+            createMyHighscore();
+        }
+        
+    }
+
+    private void createMyHighscore(){
+        TableLayout tl = (TableLayout) findViewById(R.id.fullscreen_content);
+
         TextView tv = new TextView(this);
-        tv.setText("Teeeeeeeeest");
-        tv.setTextSize(20);
-        tr.addView(tv);
-        tl.addView(tr);
-        //löschen
-        tl.removeView(tr);
+        tv.setTextSize(40);
+        tv.setText("My Highscore");
+        tv.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
 
+        TableRow tr = new TableRow(this);
+        tr.setBackgroundColor(Color.RED);
+        tr.getBackground().setAlpha(127);
+        Log.d("Meine Randomzahl: ", "" + sessionID);
+        HighscoreMockup me = hmt.getMyHighscore(sessionID);
 
-        //szenario 2
         TextView tv2 = new TextView(this);
-        tv2.setText("Teeeeeeeeest2");
         tv2.setTextSize(20);
-        tr.addView(tv2);
-        tl.addView(tr);
-        tl.removeView(tr);
+        tv2.setText("" + me.getRanking());
+        tv2.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
 
-
-        //szenario 3
         TextView tv3 = new TextView(this);
-        tv3.setText("Teeeeeeeeest2");
         tv3.setTextSize(20);
-        tr.addView(tv3);
-        tl.addView(tr);
-        //tl.removeView(tr);
-        //-------------------------------------
-        */
+        tv3.setText(me.getName());
+        tv3.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
 
+        TextView tv4 = new TextView(this);
+        tv4.setTextSize(20);
+        tv4.setText("" + me.getScore());
+        tv4.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+
+        tl.addView(tv);
+        tr.addView(tv2);
+        tr.addView(tv3);
+        tr.addView(tv4);
+        tl.addView(tr);
     }
 
     HighscoreMockupTest hmt;
 
+    /*
     public void getTop10(View v){
 
         for (HighscoreMockup hm: hmt.getTop10()) {
             Log.d("Datensatz:", hm.toString());
         }
 
-        hierVersucheIchRowsZuErstellen(v, hmt.getTop10());
+        hierVersucheIchRowsZuErstellen(hmt.getTop10());
     }
 
     public void addUser(View v){
@@ -115,4 +139,38 @@ public class BoardActivity extends AppCompatActivity {
         hm.setRanking(2);
         hmt.addNewUser(hm);
     }
+    */
+
+    /* 02.05.2016 */
+    class HighscoreDataTask extends AsyncTask<String, Void, ArrayList>{
+
+        private ProgressDialog pd = new ProgressDialog(BoardActivity.this);
+
+        @Override
+        protected void onPreExecute(){
+            this.pd.setMessage("Loading Highscores");
+            this.pd.setIndeterminate(false);
+            this.pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            this.pd.setCancelable(true);
+            this.pd.show();
+        }
+
+        protected ArrayList<HighscoreMockup> doInBackground(String... params){
+
+            try {
+                Thread.sleep(5000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            return hmt.getTop10();
+        }
+
+        protected void onPostExecute(ArrayList top10){
+            //super.onPostExecute(result);
+            this.pd.dismiss();
+            hierVersucheIchRowsZuErstellen(top10);
+        }
+    }
+
 }
--- app/src/main/java/seng1/rockpapertoe/HighscoreMockupTest.java
+++ app/src/main/java/seng1/rockpapertoe/HighscoreMockupTest.java
@@ -8,40 +8,95 @@ import java.util.Collections;
  */
 public class HighscoreMockupTest {
 
-    private HighscoreMockup a,b,c;
+    private HighscoreMockup a,b,c,d,e,f,g,h,i,j,k;
 
-    ArrayList<HighscoreMockup> highscores;
+    private ArrayList<HighscoreMockup> highscores;
+
 
     public HighscoreMockupTest() {
         a = new HighscoreMockup();
         b = new HighscoreMockup();
         c = new HighscoreMockup();
+        d = new HighscoreMockup();
+        e = new HighscoreMockup();
+        f = new HighscoreMockup();
+        g = new HighscoreMockup();
+        h = new HighscoreMockup();
+        i = new HighscoreMockup();
+        j = new HighscoreMockup();
+        k = new HighscoreMockup();
 
         // Szenario a
         a.setId(1);
         a.setName("Killer");
         a.setRanking(1);
-        a.setScore(10);
+        a.setScore(100);
 
         // Szenario b
         b.setId(2);
         b.setName("Joo");
-        b.setRanking(10);
-        b.setScore(1);
+        b.setRanking(2);
+        b.setScore(99);
 
         // Szenario c
         c.setId(3);
         c.setName("returrrn");
         c.setRanking(3);
-        c.setScore(4);
+        c.setScore(90);
+
+        d.setId(4);
+        d.setName("Detlef");
+        d.setRanking(4);
+        d.setScore(85);
+
+        e.setId(5);
+        e.setName("Johann");
+        e.setRanking(5);
+        e.setScore(82);
+
+        f.setId(6);
+        f.setName("Max");
+        f.setRanking(6);
+        f.setScore(80);
+
+        g.setId(7);
+        g.setName("Tom");
+        g.setRanking(7);
+        g.setScore(78);
+
+        h.setId(8);
+        h.setName("Andre");
+        h.setRanking(8);
+        h.setScore(59);
+
+        i.setId(9);
+        i.setName("Kevin");
+        i.setRanking(9);
+        i.setScore(40);
+
+        j.setId(10);
+        j.setName("Terminator");
+        j.setRanking(10);
+        j.setScore(20);
+
+        k.setId(11);
+        k.setName("Ich bin nicht in der Top 10");
+        k.setRanking(11);
+        k.setScore(15);
 
         highscores = new ArrayList<>();
 
         highscores.add(c);
         highscores.add(b);
         highscores.add(a);
-
-        //Collections.sort(highscores);
+        highscores.add(j);
+        highscores.add(i);
+        highscores.add(g);
+        highscores.add(d);
+        highscores.add(f);
+        highscores.add(e);
+        highscores.add(h);
+        highscores.add(k);
     }
 
     public ArrayList<HighscoreMockup> getTop10(){
@@ -49,12 +104,28 @@ public class HighscoreMockupTest {
 
         ArrayList<HighscoreMockup> top10 = new ArrayList<>();
 
-        for(int i = 0; i < highscores.size(); i++){
+        int size = 0;
+
+        if(highscores.size() < 10)
+            size = highscores.size();
+        else
+            size = 10;
+
+        for(int i = 0; i < size; i++){
             if(i < 10)
                 top10.add(highscores.get(i));
         }
 
         return top10;
+    }
+
+    public HighscoreMockup getMyHighscore(int id){
+        if(id > 0)
+            for(int i=0; i < highscores.size(); i++){
+                if(highscores.get(i).getId() == id)
+                    return highscores.get(i);
+            }
+        return null;
     }
 
     public void addNewUser(HighscoreMockup hm){
